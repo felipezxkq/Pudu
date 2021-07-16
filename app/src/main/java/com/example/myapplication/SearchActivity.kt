746dd73b67eff88aa6965b2822469889995b61b0
Diegoj95 <diegojimenez377@gmail.com>
2021-07-16 18:05:19 -0400

    Facebook api connection, login and simple share button

--- app/src/main/java/com/example/myapplication/SearchActivity.kt
+++ app/src/main/java/com/example/myapplication/SearchActivity.kt
@@ -3,16 +3,27 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivitySearchBinding
+import com.facebook.*
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.zxing.integration.android.IntentIntegrator
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 
 class SearchActivity : AppCompatActivity() {
 
+    var nombreProducto: String = "Vacio"
+
+    private var callbackManager:CallbackManager? = null
+
     private lateinit var binding:ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
@@ -24,7 +35,48 @@ class SearchActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+
+
+        val shareBtn = findViewById<Button>(R.id.shareBtn)
+        shareBtn.setOnClickListener {
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, nombreProducto)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Please select app: "))
+        }
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        val loginButton = findViewById<LoginButton>(R.id.login_button)
+        loginButton.setReadPermissions("email")
+        // If using in a fragment
+        // If using in a fragment
+        //loginButton.setFragment(this)
+
+        // Callback registration
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                var accessToken = AccessToken.getCurrentAccessToken()
+                var isLoggedIn = accessToken != null && !accessToken!!.isExpired
+                Log.d("ACCESS-TOKEN", accessToken.token)
+            }
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+        })
     }
+
     private fun initScanner(){
         val integrator = IntentIntegrator(this)
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
@@ -45,6 +97,10 @@ class SearchActivity : AppCompatActivity() {
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+
     }
 
     private fun returnProducts(code: String){
@@ -55,6 +111,8 @@ class SearchActivity : AppCompatActivity() {
             .addOnSuccessListener { documents ->
                 if(documents.first() != null){
                     productText.setText("${documents.first().data.getValue("product_name")}")
+                    nombreProducto = ("${documents.first().data.getValue("product_name")}")
+
                 }
                 else{
                     productText.setText("Producto no encontrado!")
@@ -64,6 +122,5 @@ class SearchActivity : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 productText.setText("Error conexión")
             }
-
     }
 }
